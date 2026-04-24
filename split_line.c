b3d376f9a8a9dc935633faa4f51d9c1c11581e35
lonie <12333@holbertonstudents.com>
2026-04-24 09:56:23 +0200

    add the final file

--- split_line.c
+++ split_line.c
@@ -1,49 +1,22 @@
 #include "shell.h"
 
-/**
- * split_line - split a modifiable line into tokens
- * @line: modifiable string (buffer from getline)
- *
- * Return: malloc'd array of pointers terminated by NULL, or NULL on error
- * Note: tokens point inside line; free(argv) is enough to free the array.
- */
 char **split_line(char *line)
 {
-	char *token;
-	char **argv;
-	int count = 0, i = 0;
-	char *p;
+    char **argv = NULL;
+    char *token;
+    int size = 0;
 
-	if (line == NULL)
-		return (NULL);
+    argv = malloc(sizeof(char *) * 64);
+    if (!argv)
+        return (NULL);
 
-	/* count tokens */
-	p = line;
-	while (*p)
-	{
-		while (*p == ' ' || *p == '\t')
-			p++;
-		if (*p == '\0')
-			break;
-		count++;
-		while (*p && *p != ' ' && *p != '\t')
-			p++;
-	}
+    token = strtok(line, " ");
+    while (token)
+    {
+        argv[size++] = token;
+        token = strtok(NULL, " ");
+    }
+    argv[size] = NULL;
 
-	if (count == 0)
-		return (NULL);
-
-	argv = malloc((count + 1) * sizeof(char *));
-	if (!argv)
-		return (NULL);
-
-	token = strtok(line, " \t");
-	while (token != NULL)
-	{
-		argv[i++] = token;
-		token = strtok(NULL, " \t");
-	}
-	argv[i] = NULL;
-
-	return (argv);
+    return (argv);
 }
