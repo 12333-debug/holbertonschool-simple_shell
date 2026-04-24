f767f89986a37f70f7524923b6da769edba7589f
lonie <12333@holbertonstudents.com>
2026-04-24 11:00:43 +0200

    add the file

--- exec.c
+++ exec.c
@@ -1,29 +1,18 @@
 #include "shell.h"
 
-/**
- * execute_command - tokenize + PATH + exec
- */
 void execute_command(char *line)
 {
+    char **argv = split_line(line);
+    char *path;
     pid_t pid;
     int status;
-    char **argv;
-    char *full_path = NULL;
 
-    if (!line)
-        return;
-
-    argv = split_line(line);
-    if (!argv)
-        return;
-
-    if (!argv[0])
+    if (!argv || !argv[0])
     {
         free(argv);
         return;
     }
 
-    /* built-in env */
     if (strcmp(argv[0], "env") == 0)
     {
         builtin_env();
@@ -31,37 +20,27 @@ void execute_command(char *line)
         return;
     }
 
-    /* PATH resolution */
-    full_path = find_path(argv[0]);
-    if (!full_path)
+    path = find_path(argv[0]);
+    if (!path)
     {
-        fprintf(stderr, "%s: command not found\n", argv[0]);
+        write(2, argv[0], strlen(argv[0]));
+        write(2, ": command not found\n", 20);
         free(argv);
         return;
     }
 
     pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        free(full_path);
-        free(argv);
-        return;
-    }
-
     if (pid == 0)
     {
-        if (execve(full_path, argv, environ) == -1)
-        {
-            perror(full_path);
-            _exit(127);
-        }
+        execve(path, argv, environ);
+        perror(path);
+        _exit(127);
     }
     else
     {
         waitpid(pid, &status, 0);
     }
 
-    free(full_path);
+    free(path);
     free(argv);
 }
